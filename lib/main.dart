cbd210d650ae2dc219f6de9bc7acff25076ebc4b
Rafael Brito <rafaelbrito.web@gmail.com>
2021-08-17 14:11:28 -0300

    Projeto para estudo de Flutter

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'TelaSecundaria.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsetsDirectional.all(32),
+        child: Column(
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsetsDirectional.all(15)
+              ),
+              onPressed: (){
+                Navigator.push(
+                  context,
+                MaterialPageRoute(builder: (context) => TelaSecundaria()),
+                );
+              },
+              child: Text("Passar para próxima tela."),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
